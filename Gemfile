ab2b074e750eda9cc3749883ba3db19224ef6d2b
dailammoc1588 <hiendinhngoc@yahoo.com.vn>
2014-02-22 01:20:05 +0700

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ source 'https://rubygems.org'
 gem 'rails', '4.0.2'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 # Use mysql as the database for Active Record
 gem 'mysql2'
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,9 +3,10 @@ module UsersHelper
   # returns the Gravatar (http://gravatar.com/) for the given user.
   # @return an image tag for the Gravatar with a "gravatar" class and alt text equal to
   # the user's name
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size:50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url,alt: user.name, class: "gravatar")
   end
 end
